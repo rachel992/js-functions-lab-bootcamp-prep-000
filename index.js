166708a666f61cd36ee82a7918ea35869db9ec60
racheliwhite01@gmail.com <racheliwhite01@gmail.com>
2018-09-13 17:46:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,8 @@ return `Happy Holidays, ${name}!`
 }
 
 function happyHolidayTo(holiday, name) {
-  
+  return `Happy ${holiday}, ${name}!`
 } 
+function holidayCountdown(holiday,days){
+  return `Its ${days} days until ${holiday}!`
+}
