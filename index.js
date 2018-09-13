d88ef0fb06596fdd8ca18db148742b22401fce50
racheliwhite01@gmail.com <racheliwhite01@gmail.com>
2018-09-13 17:52:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 } 
 function holidayCountdown(days, holiday) {
-  return `It\'s ${days} days until ${holiday}!`
+  return `It\'s ${20} days until ${'Mother\'s Day'}!`
 }
